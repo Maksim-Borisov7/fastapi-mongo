520774c97a00c09de5ec5f725ce092b9d7f5458b
Maxim-Borisov7 <maxvadbor@mail.ru>
2025-05-16 21:39:15 +0500

    change db_helper

--- app/database/db_helper.py
+++ app/database/db_helper.py
@@ -1,11 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import Request
 
+from app.config import settings
+
 
 class DataBaseHelper:
-    def __init__(self, mongodb_url: str = 'mongodb://localhost:27017'):
+    def __init__(self, mongodb_url: str = f'mongodb://{settings.MONGO_HOST}:{settings.MONGO_PORT}'):
         self.client = AsyncIOMotorClient(mongodb_url)
-        self.dbs = self.client['shop']
+        self.dbs = self.client[settings.MONGO_DB]
 
     def get_collection(self, collection: str):
         return self.dbs[collection]
